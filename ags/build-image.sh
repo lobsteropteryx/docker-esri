2c93edf3666e0af0cd8b49f11cce568323dc8f50
Ian Firkin <ian.firkin@gisinc.com>
2016-12-13 17:53:37 -0500

    Refactors scripts and adds 10.5 files
    Refactors to pass installer and prvc files into build script, instead of
    dockerfile.

--- ags/build-image.sh
+++ ags/build-image.sh
@@ -1,5 +1,7 @@
 docker build\
+  --build-arg PRVC=ArcGIS_ServerStandardEnterprise_EDN_2017.prvc \
+  --build-arg AGS_ZIP=ArcGIS_Server_Linux_105_pr_153237.tar.gz \
   --force-rm=true\
   --ulimit nofile=65535:65535\
   --ulimit nproc=25059:25059\
-  -t ags10.3 .
+  -t ags10.5 .
