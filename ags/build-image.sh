781085c8d1f123547925a1ee23d11ac5902390f5
Ian Firkin <ian.firkin@gisinc.com>
2016-12-19 09:00:27 -0500

    Adds BUILD_HOST argument

--- ags/build-image.sh
+++ ags/build-image.sh
@@ -1,4 +1,5 @@
 docker build\
+  --build-arg BUILD_HOST=$COMPUTERNAME.$USERDNSDOMAIN:8000 \
   --build-arg PRVC=ArcGIS_ServerStandardEnterprise_EDN_2017.prvc \
   --build-arg AGS_INSTALLER=ArcGIS_Server_Linux_105_pr_153237.tar.gz \
   --force-rm=true\
