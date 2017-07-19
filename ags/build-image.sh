8e433c44ebd5935bbae3a75f57ff1a225eb88350
Ian Firkin <ian.firkin@gisinc.com>
2017-07-18 22:39:32 -0400

    WIP fixing permissions

--- ags/build-image.sh
+++ ags/build-image.sh
@@ -1,7 +1,7 @@
-docker build\
-  --build-arg BUILD_HOST=$COMPUTERNAME.$USERDNSDOMAIN:8000\
+docker build \
+  --build-arg BUILD_HOST=192.168.56.1:8000 \
   --build-arg AGS_INSTALLER=ArcGIS_for_Server_Linux_104_149446.tar.gz \
-  --force-rm=true\
-  --ulimit nofile=65535:65535\
-  --ulimit nproc=25059:25059\
+  --force-rm=true \
+  --ulimit nofile=65535:65535 \
+  --ulimit nproc=25059:25059 \
   -t ags10.4 .
