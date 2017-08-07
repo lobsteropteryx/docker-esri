27441404cee639bfc4cf6bcd65f91c3daab802d4
Ian Firkin <ian.firkin@gisinc.com>
2017-08-06 22:05:46 -0400

    Updates provisioning file

--- ags/build-image.sh
+++ ags/build-image.sh
@@ -1,6 +1,6 @@
 docker build\
-  --build-arg BUILD_HOST=192.168.56.1:8000 \
-  --build-arg PRVC=ArcGISGISServerAdvanced_ArcGISServer_526454.prvc \
+  --build-arg BUILD_HOST=192.168.1.47:8000 \
+  --build-arg PRVC=ArcGISGISServerAdvanced_ArcGISServer_547696.prvc \
   --build-arg AGS_INSTALLER=ArcGIS_Server_Linux_105_pr_153237.tar.gz \
   --force-rm=true\
   --ulimit nofile=65535:65535\
