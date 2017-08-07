dd2550491e3e9cd58e991a8038201f3899ff3336
Ian Firkin <ian.firkin@gisinc.com>
2017-08-06 22:25:32 -0400

    Changes to 10.4

--- ags/build-image.sh
+++ ags/build-image.sh
@@ -1,8 +1,8 @@
 docker build\
   --build-arg BUILD_HOST=192.168.1.47:8000 \
-  --build-arg PRVC=ArcGISGISServerAdvanced_ArcGISServer_547696.prvc \
-  --build-arg AGS_INSTALLER=ArcGIS_Server_Linux_105_pr_153237.tar.gz \
+  --build-arg PRVC=ArcGISforServerAdvancedEnterprise_Server_547695.prvc \
+  --build-arg AGS_INSTALLER=ArcGIS_for_Server_Linux_104_149446.tar.gz \
   --force-rm=true\
   --ulimit nofile=65535:65535\
   --ulimit nproc=25059:25059\
-  -t ags10.5 .
+  -t ags10.4 .
