b7732d57e232b75f85ea75a29df6b97088bae94c
Ian Firkin <ian.firkin@gisinc.com>
2017-08-06 22:33:42 -0400

    Removes postgres stuff

--- postgres/build-image.sh
+++
@@ -1,5 +0,0 @@
-docker build\
-  --force-rm=true\
-  --ulimit nofile=65535:65535\
-  --ulimit nproc=25059:25059\
-  -t postgres .
--- postgres/run.sh
+++
@@ -1,6 +0,0 @@
-docker run \
-    -it --rm \
-    --hostname arcgis \
-    --memory-swappiness=0 \
-    -p 6080:6080 \
-postgres
