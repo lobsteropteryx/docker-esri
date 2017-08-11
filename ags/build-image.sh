93b982c47683a615d2249ad556931f77a8ac428c
ifirkin <ian.firkin@homeinsteadinc.com>
2017-08-10 21:10:06 -0400

    Makes scripts executable

--- ags/build-image.sh
+++ ags/build-image.sh
@@ -1,5 +1,5 @@
 docker build\
-  --build-arg BUILD_HOST=192.168.1.47:8000 \
+  --build-arg BUILD_HOST=192.168.1.197:8000 \
   --build-arg PRVC=ArcGISforServerAdvancedEnterprise_Server_547695.prvc \
   --build-arg AGS_INSTALLER=ArcGIS_for_Server_Linux_104_149446.tar.gz \
   --force-rm=true\
